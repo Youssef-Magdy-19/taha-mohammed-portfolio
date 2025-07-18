6741bb2174945b19a11ce7f08753c102b49b745a
Youssef-Magdy-19 <youssefostora1155@gmail.com>
2025-07-18 22:16:23 +0300

    update about img

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 import { motion } from "framer-motion";
 import { CheckCircle, Award, PhoneCall } from "lucide-react";
 // @ts-ignore
-import whyChooseUs from "/images/whychoose.jpeg"
+const whyChooseUs = "/images/whychoose.jpeg"
 import useWindowScrollToTop from "../hooks/useWindowScrollToTop";
 import { HashLink } from "react-router-hash-link";
 import { useLoading } from "../context/LoadingContext";
